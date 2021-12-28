0bc8cd58c28cbe67837491bbd61d7e78bf08df9a
liujunjie <liujunjiejob@126.com>
2021-12-28 21:22:59 +0800

    Spring 由构造函数自动装配

--- src/main/java/com/qingyu/spring/autowire/byName/TextEditor.java
+++ src/main/java/com/qingyu/spring/autowire/byName/TextEditor.java
@@ -5,6 +5,11 @@ public class TextEditor {
     private SpellChecker spellChecker;
     private String name;
 
+    public TextEditor(SpellChecker spellChecker ,  String name) {
+        this.spellChecker = spellChecker;
+        this.name = name;
+    }
+
     public SpellChecker getSpellChecker() {
         return spellChecker;
     }
