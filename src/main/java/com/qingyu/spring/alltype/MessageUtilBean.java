115db64d20179d42d096286b4d398a9311c54d73
liujunjie <liujunjiejob@126.com>
2022-01-03 13:07:13 +0800

    各种类型信息的注入配置格式.

--- src/main/java/com/qingyu/spring/alltype/MessageUtilBean.java
+++ src/main/java/com/qingyu/spring/alltype/MessageUtilBean.java
@@ -6,23 +6,13 @@ import java.util.Map;
 import java.util.Properties;
 import java.util.Set;
 
-public class MessageBean {
+public class MessageUtilBean {
     private String name;
     private int age;
-    private Date birth;
     private List<String> friends;
     private Set<String> cities;
-    private String remark;
     private Map<String, String> books;
     private Properties db;
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 
     public void setAge(int age) {
         this.age = age;
@@ -30,15 +20,6 @@ public class MessageBean {
 
     public void setFriends(List<String> friends) {
         this.friends = friends;
-    }
-
-    public void setBirth(String birth) {
-
-        this.birth = Date.valueOf(birth);
-    }
-
-    public void setRemark(String remark) {
-        this.remark = remark;
     }
 
     public void setCities(Set<String> cities) {
@@ -53,15 +34,17 @@ public class MessageBean {
         this.db = db;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
     @Override
     public String toString() {
-        return "MessageBean{" +
+        return "MessageUtilBean{" +
                 "name='" + name + '\'' +
                 ", age=" + age +
-                ", birth=" + birth +
                 ", friends=" + friends +
                 ", cities=" + cities +
-                ", remark='" + remark + '\'' +
                 ", books=" + books +
                 ", db=" + db +
                 '}';
--- src/test/java/com/qingyu/spring/testinit/allType/testAllType.java
+++ src/test/java/com/qingyu/spring/testinit/allType/testAllType.java
@@ -1,6 +1,7 @@
 package com.qingyu.spring.testinit.allType;
 
 import com.qingyu.spring.alltype.MessageBean;
+import com.qingyu.spring.alltype.MessageUtilBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -9,5 +10,7 @@ public class testAllType {
         ApplicationContext ac =new ClassPathXmlApplicationContext("applicationContext.xml");
         MessageBean messageBean=  ac.getBean("messageBean", MessageBean.class);
         System.out.println(messageBean.toString());
+        MessageUtilBean messageUtilBean=  ac.getBean("messageUtilBean", MessageUtilBean.class);
+        System.out.println(messageUtilBean.toString());
     }
 }
